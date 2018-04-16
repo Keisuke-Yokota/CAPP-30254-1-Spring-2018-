842305a9bbd9784fce55dafd984c2404bc010b22
Keisuke Yokota <keisukey@cs.uchicago.edu>
2018-04-15 21:53:46 -0500

    omg

--- hw2/Analysis.py
+++ hw2/Analysis.py
@@ -7,11 +7,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 ### 1 Read Data
-def read(filename):
-    df = pd.read_csv(filename, header=0)
-    df = df.rename(columns={'Unnamed: 0':'ID'})
-    df = df.set_index('ID')
-    return df
+def load_data(fn1='cs-training.csv', fn2='cs-test.csv'):
+    df1 = pd.read_csv(fn1)
+    df1 = df1.rename(columns={'Unnamed: 0':'ID', 
+                        'NumberOfTime30-59DaysPastDueNotWorse':'Less2months',
+                       'NumberOfTimes90DaysLate':'More3months',
+                       'NumberOfTime60-89DaysPastDueNotWorse':'Less3months'})
+    df1 = df1.set_index('ID')
+    df2 = pd.read_csv(fn2)
+    df2 = df2.rename(columns={'Unnamed: 0':'ID', 
+                        'NumberOfTime30-59DaysPastDueNotWorse':'Less2months',
+                       'NumberOfTimes90DaysLate':'More3months',
+                       'NumberOfTime60-89DaysPastDueNotWorse':'Less3months'})
+    df2 = df2.set_index('ID')
+    return df1, df2
 
 ### 2 Explore Data
 def plot(dataframe, column_name):
@@ -37,4 +46,46 @@ def plot(dataframe, column_name):
     plt.scatter(X1[:,0], X1[:,1], color='blue', marker='x', label='ONE')
     plt.legend(loc='upper left')
     plt.show()
+
+
+    sns.distplot(df.dropna(subset=['NumberOfDependents']).NumberOfDependents,kde = True)
+plt.show()
     '''
+
+### 3 Pre-process Data
+from scipy import stats
+def adj_outlier(dataframe,column_name):
+    target = dataframe[column_name]
+    q1 = stats.scoreatpercentile(target, 25)
+    q3 = stats.scoreatpercentile(target, 75) 
+    target_iqr = q3 - q1
+
+    target_iqr_min = q1 - (target_iqr) * 1.5
+    target_iqr_max = q3 + (target_iqr) * 1.5
+    df = dataframe[target < target_iqr_max]
+    return df
+
+def outlier_iqr(df):
+    df0 = df[:]
+    for i in range(1,len(df0.columns)):
+        col = df.iloc[:,i]
+        q1 = stats.scoreatpercentile(target, 25)
+        q3 = stats.scoreatpercentile(target, 75) 
+        iqr = q3 - q1
+        outlier_min = q1 - (iqr) * 1.5
+        outlier_max = q3 + (iqr) * 1.5
+        col[col < outlier_min] = None
+        col[col > outlier_max] = None
+    return df0
+
+
+def outlier(dataframe):
+    df = dataframe
+    for column in df.columns:
+        new_df = adj_outlier(df,column)
+        df = new_df
+    return df
+    '''
+    df['MonthlyIncome'] = df['MonthlyIncome'].fillna(df['MonthlyIncome'].median())
+    df['NumberOfDependents'] = df['NumberOfDependents'].fillna(df['NumberOfDependents'].median())
+    '''  
